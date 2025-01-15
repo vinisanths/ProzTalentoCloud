4b30dc456948d5599cdae210154b95d2723587e3
vinisanths <vinipsantos0@gmail.com>
2025-01-15 15:07:12 -0300

    Adição do arquivo Arrays1.py com cardápio de lanchonete e lista de anos de nascimento

---
+++ Python/ExerciciosPython/Arrays1.py
@@ -0,0 +1,5 @@
+cardapioLanchonete = ["pizza", "hamburguer", "batata frita", "refrigerante", "sorvete"]
+print(cardapioLanchonete)
+
+anosDeNascimento = [2000, 1998, 2003, 2005, 1999]
+print(anosDeNascimento)
