93087efb6621769f05b1272fef0c2a8f3c0cc0b8
vinisanths <vinipsantos0@gmail.com>
2025-01-15 16:04:48 -0300

    Adição do arquivo Arrays4.py com manipulação de lista de produtos de beleza

---
+++ Python/ExerciciosPython/Arrays4.py
@@ -0,0 +1,12 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+print(lista_produtos)
+
+lista_produtos.pop(1)
+lista_produtos.append('Rimeis')
+lista_produtos.pop(3)
+lista_produtos.append('hidratantes')
+print(lista_produtos)
+lista_produtos.append('demaquilantes')
+lista_produtos.append('protetores solares')
+print(lista_produtos)
