939020a6045b2e41e98406b5ee8ac598427765d6
vinisanths <vinipsantos0@gmail.com>
2025-01-15 16:10:11 -0300

    Atualização da lista de produtos de beleza: substituição de itens e remoção de um item obsoleto

--- Python/ExerciciosPython/Arrays4.py
+++ Python/ExerciciosPython/Arrays4.py
@@ -2,11 +2,11 @@ lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loçõ
 
 print(lista_produtos)
 
-lista_produtos.pop(1)
-lista_produtos.append('Rimeis')
-lista_produtos.pop(3)
-lista_produtos.append('hidratantes')
-print(lista_produtos)
+lista_produtos[1] = 'Rimel'
+lista_produtos[4] = 'Creme Hidratante'
+lista_produtos.pop()
+
+
 lista_produtos.append('demaquilantes')
 lista_produtos.append('protetores solares')
 print(lista_produtos)
