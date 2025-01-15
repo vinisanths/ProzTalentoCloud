a4ffacdc1f72632ec6035c7498b49e3476a33bed
vinisanths <vinipsantos0@gmail.com>
2025-01-15 15:48:04 -0300

    adição do arquivo Arrays3.py

---
+++ Python/ExerciciosPython/Arrays3.py
@@ -0,0 +1,4 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+for i in range(len(lista_produtos)):
+    print(f"Temos {lista_produtos[i]} à venda")
